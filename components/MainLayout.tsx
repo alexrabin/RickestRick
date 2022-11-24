a5353b1aa9e1c001ba1e98ede2e47cea456616c1
Alex Rabin <alexrabin@me.com>
2022-11-23 21:57:15 -0700

    Bare bones set up for rick-est rick

---
+++ components/MainLayout.tsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import styled from "styled-components";
+
+const LayoutContainer = styled.div`
+  min-height: 100vh;
+  padding: 4rem 1rem;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+type Props = {
+  documentTitle?: string;
+  description?: string;
+  children?: JSX.Element | JSX.Element[];
+};
+
+const MainLayout: React.FC<Props> = ({
+  documentTitle = null,
+  children,
+  description = "Are you the Rick-est Rick?",
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>
+          {documentTitle && documentTitle.length > 0
+            ? `${documentTitle} - Rick-est Rick`
+            : "Rick-est Rick"}
+        </title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <LayoutContainer>{children}</LayoutContainer>
+    </div>
+  );
+};
+export default MainLayout;
---
+++ services/apiService.ts
@@ -0,0 +1,69 @@
+import axios from "axios";
+
+const baseUrl = "https://rickandmortyapi.com/api/";
+const RickMortyAPI = axios.create({
+  baseURL: baseUrl,
+  responseType: "json",
+  headers: {
+    "accept-encoding": "*",
+  },
+});
+
+export const getAllCharacters = async (page = 1) => {
+  try {
+    const res = await RickMortyAPI.get(`character/?page=${page}`);
+    return res.data;
+  } catch (e) {
+    return null;
+  }
+};
+
+export const getCharacter = async (id: string) => {
+  try {
+    const res = await RickMortyAPI.get(`character/?${id}`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+export const getAllLocations = async (page = 1) => {
+  try {
+    const res = await RickMortyAPI.get(`location/?page=${page}`);
+    return res.data;
+  } catch (e) {
+    return null;
+  }
+};
+
+export const getLocation = async (id: string) => {
+  try {
+    const res = await RickMortyAPI.get(`location/${id}`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+export const getAllEpisodes = async () => {
+  try {
+    const res = await RickMortyAPI.get(`episode`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+export const getEpisode = async (id: string) => {
+  try {
+    const res = await RickMortyAPI.get(`episode/${id}`);
+
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};

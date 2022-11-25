c2d1a6581d0d490c924448b36d2a3e59d048418a
Alex Rabin <alexrabin@me.com>
2022-11-25 13:11:17 -0700

    Removed rogue ';'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextNProgress />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
